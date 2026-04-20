e46646afef8f3ddbfc95afd78fe1ec98aa610758
Diaa Ashraf <elshafeydiaa@gmail.com>
2026-04-20 23:29:14 +0200

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Sanctum للـ API
         $middleware->api(prepend: [
             \Illuminate\Http\Middleware\HandleCors::class,
-        \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
     })
