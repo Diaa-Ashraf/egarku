624e7ad9ef0d7ba9bb4aada99f6d1a8ff89b2e7e
Diaa Ashraf <elshafeydiaa@gmail.com>
2026-08-26 02:39:02 +0300

    update notification

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,8 +30,6 @@ class AdminPanelProvider extends PanelProvider
             ->authGuard('admin')
             ->login()
             ->colors(['primary' => Color::Blue])
-            ->databaseNotifications()
-            ->databaseNotificationsPolling('5s')
 
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
